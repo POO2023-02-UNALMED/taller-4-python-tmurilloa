6c640491d325bdfb0ea2dd8bde8a285a99176e6f
tmurilloa <tmurilloa@unal.edu.co>
2023-09-03 11:03:20 -0500

    actualizacion 1

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,10 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon = "remoto"):
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        cadena = f"{self._nombre} {self._salon}"
+        return cadena
+    
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,23 +1,31 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes = None):
         self._grupo = grupo
+        if asignaturas is None:
+            asignaturas = []
         self._asignaturas = asignaturas
+        if estudiantes is None:
+            estudiantes = []
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista= None):
+        if lista is None:
+            lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
-    # def __str__(self):
-    #     pass
+   
+    def __str__(self):
+        cadena = f"Grupo de estudiantes: {self._grupo}"
+        return cadena
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
@@ -28,5 +36,5 @@ class Grupo:
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
